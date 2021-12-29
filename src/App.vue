a2f4e6cccd8fb110da40995427f5832cdad46a46
Abraham Peter <theclassical001@gmail.com>
2021-12-29 16:43:08 +0100

    toggled div created

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 
 <div class="container">
    <Header title = "Task Tracker" />
-   <Tasks @delete-task="deleteTask" :tasks="tasks"/>
+   <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks"/>
 </div>
   
 </template>
@@ -28,6 +28,10 @@ export default {
         this.tasks = this.tasks.filter((task) => task.id !== id)
       }
     },
+    toggleReminder(id) {
+      this.tasks = this.tasks.map((task) => task.id === id ? 
+      { ...task, reminder: !task.reminder } : task)
+    }
   },
   created() {
     this.tasks = [
